4a7156cfea71acd2eb7a2e75933e924fe1886194
Andrey Sokolov <a.sokolov@iblschool.ru>
2025-08-17 16:30:33 +0300

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,11 @@ import { ServerExceptionFilter } from './filter/server-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: true, // Разрешает запросы со всех доменов
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       // трансформирование входящего json в экзмепляр класса в соответствии с dto
